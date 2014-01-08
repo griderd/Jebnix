2a2532b96ddb393a4992205f98651866907fbcde
DavidGrider <woodywood245@yahoo.com>
2014-01-07 19:58:26 -0800

    Added members and documentation to ExecutionState.

--- KerbalScriptEngine/ExecutionState.cs
+++ KerbalScriptEngine/ExecutionState.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace KerbalScriptEngine
 {
+    /// <summary>
+    /// Represents the execution state of a script.
+    /// </summary>
     class ExecutionState
     {
         public enum Status
@@ -17,6 +20,75 @@ namespace KerbalScriptEngine
             WhenBlock
         }
 
+        /// <summary>
+        /// Lines of the currently-executing script.
+        /// </summary>
+        public List<LineInfo> lines;
 
+        /// <summary>
+        /// Next line to execute.
+        /// </summary>
+        public int nextLine;
+
+        /// <summary>
+        /// Next token to execute.
+        /// </summary>
+        public int nextToken;
+
+        /// <summary>
+        /// Stack of scope-based local values.
+        /// </summary>
+        public Stack<Dictionary<string, Value>> scopeStack;
+
+        /// <summary>
+        /// Stack of internal call frames.
+        /// </summary>
+        public Stack<ExecutionFrame> executionStack;
+
+        /// <summary>
+        /// Stack of execution statuses
+        /// </summary>
+        public Stack<Status> statusStack;
+
+        /// <summary>
+        /// When events by condition, where value is a Tuple containing the line number and token ID.
+        /// </summary>
+        public Dictionary<LineInfo, Tuple<int, int>> whenBlocks;
+
+        /// <summary>
+        /// When events by condition, where value is a boolean determining if the event persists.
+        /// </summary>
+        public Dictionary<LineInfo, bool> whenThenPersist;
+
+        /// <summary>
+        /// Function parameters
+        /// </summary>
+        public Dictionary<string, Value> parameters;
+
+        public ExecutionState(LineInfo[] lines)
+        {
+            this.lines = new List<LineInfo>(lines);
+            nextLine = 0;
+            nextToken = 0;
+            scopeStack = new Stack<Dictionary<string, Value>>();
+            executionStack = new Stack<ExecutionFrame>();
+            statusStack = new Stack<Status>();
+            whenBlocks = new Dictionary<LineInfo, Tuple<int,int>>();
+            whenThenPersist = new Dictionary<LineInfo, bool>();
+            parameters = new Dictionary<string, Value>();
+            scopeStack.Push(new Dictionary<string, Value>());
+        }
+
+        public void PushExecutionFrame()
+        {
+            executionStack.Push(new ExecutionFrame(nextLine, nextToken));
+        }
+
+        public void PopExecutionFrame()
+        {
+            ExecutionFrame f = executionStack.Pop();
+            nextLine = f.LineNumber;
+            nextToken = f.TokenOffset;
+        }
     }
 }
