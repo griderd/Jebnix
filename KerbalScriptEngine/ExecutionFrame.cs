6a2b4efd0250cc6ab81d832722c55619042ae88f
DavidGrider <woodywood245@yahoo.com>
2014-01-07 19:57:33 -0800

    Created class.

---
+++ KerbalScriptEngine/ExecutionFrame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerbalScriptEngine
+{
+    class ExecutionFrame
+    {
+        public int LineNumber { get; private set; }
+        public int TokenOffset { get; private set; }
+
+        public ExecutionFrame(int lineNumber, int tokenOffset)
+        {
+            LineNumber = lineNumber;
+            TokenOffset = tokenOffset;
+        }
+    }
+}
