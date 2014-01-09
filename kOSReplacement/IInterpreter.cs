7825b67b6360855473b8e307b35d4cc11a237dd3
DavidGrider <woodywood245@yahoo.com>
2014-01-08 17:38:47 -0800

    Added interface for Jebnix/scripting engine interactivity

---
+++ kOSReplacement/IInterpreter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Jebnix
+{
+    public interface IInterpreter
+    {
+        bool CreateProcess(string file);
+        bool CreateProcess(string[] lines, string filename);
+        void ExecuteProcess();
+        bool HasVariable(string name);
+        void MemoryDump();
+    }
+}
