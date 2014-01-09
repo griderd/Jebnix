78cb70b559edaa495802e134c705efeaba89cde5
DavidGrider <woodywood245@yahoo.com>
2014-01-08 23:28:44 -0800

    Added

---
+++ KerbalScriptEngine/IInterpreter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace KerboScriptEngine
+{
+    interface IInterpreter
+    {
+        bool CreateProcess(string file);
+        bool CreateProcess(string[] lines, string filename);
+        void ExecuteProcess();
+        Value GetVariable(string name);
+        bool HasVariable(string name);
+        void MemoryDump();
+        void SetVariable(string name, Value val);
+        bool TryGetVariable(string name, out Value value);
+    }
+}
