64e9e7ccf32d2007b9b51e55c0e9ad128d28bcc6
DavidGrider <woodywood245@yahoo.com>
2014-02-03 01:36:39 -0800

    Added PI and E, inverse trig functions, and modified code to take advantage of implicit conversion to Value.

--- KerbalScriptEngine/ScriptAPI/KerboMath.cs
+++ KerbalScriptEngine/ScriptAPI/KerboMath.cs
@@ -7,17 +7,50 @@ namespace KerboScriptEngine.ScriptAPI
 {
     class KerboMath
     {
+        /// <summary>
+        /// The constant PI
+        /// </summary>
+        public static Value Pi
+        {
+            get
+            {
+                return Math.PI;
+            }
+        }
+
+        /// <summary>
+        /// The constant natural number E
+        /// </summary>
+        public static Value E
+        {
+            get
+            {
+                return Math.E;
+            }
+        }
+
+        /// <summary>
+        /// Gets the absolute value of a number
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value Abs(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
             if (x.Type == Value.ValueTypes.Integer)
-                return new Value(x.IntegerValue < 0 ? x.IntegerValue * -1 : x.IntegerValue);
+                return x.IntegerValue < 0 ? x.IntegerValue * -1 : x.IntegerValue;
             else
-                return new Value(x.FloatValue < 0 ? x.FloatValue * -1 : x.FloatValue);
+                return x.FloatValue < 0 ? x.FloatValue * -1 : x.FloatValue;
         }
 
+        /// <summary>
+        /// Gets the modulus of two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static Value Mod(Value a, Value b)
         {
             if (Value.IsNull(a) | Value.IsNull(b))
@@ -33,60 +66,96 @@ namespace KerboScriptEngine.ScriptAPI
             }
         }
 
+        /// <summary>
+        /// Gets the floor of a number
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value Floor(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(x.IntegerValue);
+            return Math.Floor(x.FloatValue);
         }
 
+        /// <summary>
+        /// Gets the ceiling of a number
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value Ceiling(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Ceiling(x.FloatValue));
+            return Math.Ceiling(x.FloatValue);
         }
 
+        /// <summary>
+        /// Rounds a number
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value Round(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Round(x.FloatValue));
+            return Math.Round(x.FloatValue);
         }
 
+        /// <summary>
+        /// Rounds a number to the given number of places after the decimal
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="place"></param>
+        /// <returns></returns>
         public static Value Round(Value x, Value place)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Round(x.FloatValue, place.IntegerValue));
+            return Math.Round(x.FloatValue, place.IntegerValue);
         }
 
+        /// <summary>
+        /// Takes the square root of a number
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value Sqrt(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Sqrt(x.FloatValue));
+            return Math.Sqrt(x.FloatValue);
         }
 
+        /// <summary>
+        /// Converts from radians to degrees
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value RadToDeg(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value((180.0 / Math.PI) * x.FloatValue);
+            return (180.0 / Math.PI) * x.FloatValue;
         }
 
+        /// <summary>
+        /// Converts from degrees to radians
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
         public static Value DegToRad(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(x.FloatValue * (Math.PI / 180.0));
+            return x.FloatValue * (Math.PI / 180.0);
         }
 
         /// <summary>
@@ -99,47 +168,112 @@ namespace KerboScriptEngine.ScriptAPI
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Sin(DegToRad(x).FloatValue));
+            return Math.Sin(DegToRad(x).FloatValue);
         }
 
+        /// <summary>
+        /// Gets the cosine of x, where x is an angle measured in degrees.
+        /// </summary>
+        /// <param name="x">An angle measured in degrees.</param>
+        /// <returns></returns>
         public static Value Cos(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Cos(DegToRad(x).FloatValue));
+            return Math.Cos(DegToRad(x).FloatValue);
         }
 
+        /// <summary>
+        /// Gets the tangent of x, where x is an angle measured in degrees.
+        /// </summary>
+        /// <param name="x">An angle measured in degrees.</param>
+        /// <returns></returns>
         public static Value Tan(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Tan(DegToRad(x).FloatValue));
+            return Math.Tan(DegToRad(x).FloatValue);
         }
 
+        /// <summary>
+        /// Gets the sine of x, where x is an angle measured in radians.
+        /// </summary>
+        /// <param name="x">An angle measured in radians.</param>
+        /// <returns></returns>
         public static Value SinR(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Sin(x.FloatValue));
+            return Math.Sin(x.FloatValue);
         }
 
+        /// <summary>
+        /// Gets the cosine of x, where x is an angle measured in radians.
+        /// </summary>
+        /// <param name="x">An angle measured in radians.</param>
+        /// <returns></returns>
         public static Value CosR(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
-            return new Value(Math.Cos(x.FloatValue));
+            return Math.Cos(x.FloatValue);
         }
 
+        /// <summary>
+        /// Gets the tangent of x, where x is an angle measured in radians.
+        /// </summary>
+        /// <param name="x">An angle measured in radians.</param>
+        /// <returns></returns>
         public static Value TanR(Value x)
         {
             if (Value.IsNull(x))
                 throw new ArgumentNullException();
 
             return new Value(Math.Tan(x.FloatValue));
+        }
+
+        public static Value ASin(Value x)
+        {
+            return RadToDeg(Math.Asin(x.FloatValue));
+        }
+
+        public static Value ACos(Value x)
+        {
+            return RadToDeg(Math.Acos(x.FloatValue));
+        }
+
+        public static Value ATan(Value x)
+        {
+            return RadToDeg(Math.Atan(x.FloatValue));
+        }
+
+        public static Value ATan2(Value y, Value x)
+        {
+            return RadToDeg(Math.Atan2(y.FloatValue, x.FloatValue));
+        }
+
+        public static Value ASinR(Value x)
+        {
+            return Math.Asin(x.FloatValue);
+        }
+
+        public static Value ACosR(Value x)
+        {
+            return Math.Acos(x.FloatValue);
+        }
+
+        public static Value ATanR(Value x)
+        {
+            return Math.Atan(x.FloatValue);
+        }
+
+        public static Value ATan2R(Value y, Value x)
+        {
+            return Math.Atan2(y.FloatValue, x.FloatValue);
         }
     }
 }
