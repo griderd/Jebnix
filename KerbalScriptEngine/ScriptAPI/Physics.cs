126846061d45adf3ea5931a55a93aa3e794bf307
DavidGrider <woodywood245@yahoo.com>
2014-02-03 14:53:08 -0800

    Added physics constants

---
+++ KerbalScriptEngine/ScriptAPI/Physics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerboScriptEngine.ScriptAPI
+{
+    class Physics
+    {
+        public static Value GravitationalConstant
+        {
+            get
+            {
+                return 6.67384E-11;
+            }
+        }
+    }
+}
