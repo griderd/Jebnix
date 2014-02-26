a8e6bb46b35b1be7b4921e51259c6c10bdff97bd
David Grider <woodywood245@yahoo.com>
2014-02-26 15:08:27 -0800

    Added Jebnix RPM

---
+++ JebnixRPM/IScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JebnixRPM
+{
+    interface IScreen
+    {
+        string Update();
+
+        void OnButtonClick(int button);
+    }
+}
---
+++ JebnixRPM/Monitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KerboScriptEngine;
+using BIOS.FileSystem;
+
+namespace JebnixRPM
+{
+    public class Monitor : InternalModule  
+    {
+        Interpreter kos;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            kos = new Interpreter(vessel, new System.IO.DirectoryInfo("PluginData\\Archive"));
+
+            base.OnLoad(node);
+        }
+
+        public string UpdateRPM(int width, int height)
+        {
+            return BIOS.Graphics.Graphics.TextMode.ToString();
+        }
+    }
+}
