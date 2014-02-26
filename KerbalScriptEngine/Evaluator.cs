44c7bbf390b284a41373e21fbadaaf490b0e4b29
David Grider <woodywood245@yahoo.com>
2014-02-26 15:32:05 -0800

    Worked on RPN evaluator.

--- KerbalScriptEngine/Evaluator.cs
+++ KerbalScriptEngine/Evaluator.cs
@@ -2,156 +2,302 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using KerboScriptEngine.InternalTypes;
 
 namespace KerboScriptEngine
 {
     class Evaluator
     {
-        public static Value Evaluate(string expression, LineInfo source, out string[] errors)
+        public static Value Evaluate(string expression, LineInfo source, Dictionary<string, Value> vars, string[] functions, out string[] errors)
         {
-            return Evaluate(new LineInfo(expression, source.Filename, source.LineNumber, source.ColumnOffset, source.Process), out errors);
+            return Evaluate(new LineInfo(expression, source.Filename, source.LineNumber, source.ColumnOffset, source.Process), vars, functions, out errors);
         }
 
-        public static Value Evaluate(LineInfo line, out string[] errors)
+        public static Value Evaluate(LineInfo line, Dictionary<string, Value> vars, string[] functions, out string[] errors)
         {
-            Stack<Value> rpn = ConvertToRPN(line.Tokens);
             List<string> err = new List<string>();
+            Stack<Value> result = new Stack<Value>();
+            string[] e;
+            Queue<string> postfix = ConvertToPostfix(line.Tokens, vars, out e);
+            err.AddRange(e);
 
-            while (rpn.Count > 1)
+            string[] unaryOperators = new string[] { "!", "~", "not", "++", "--" };
+
+            while (postfix.Count > 0)
             {
-                string operation;
-                bool unary = false;
-                Value b = rpn.Pop();
-                string bname = "";
-                string aname = "";
-
-                if (b.Type == Value.ValueTypes.Pointer)
+                string token = postfix.Dequeue();
+                if (ReservedWords.Operators.Contains(token))
                 {
-                    bname = b.StringValue;
-                    if (!line.Process.TryGetVariable(b.StringValue, out b))
+                    Value a, b;
+                    if (result.Count > 0)
+                        b = result.Pop();
+                    else
                     {
-                        ErrorBuilder.BuildError(line, ErrorBuilder.ErrorType.RuntimeError, "Variable \"" + b.StringValue + "\" undeclared.", ref err);
+                        err.Add("Syntax error: invalid expression.");
                         errors = err.ToArray();
-                        return Value.NullValue;
+                        return 0;
                     }
-                }
 
-                Value a = rpn.Pop();
-                if (ReservedWords.Operators.Contains(a.StringValue))
-                {
-                    operation = a.StringValue;
-                    unary = true;
-                }
-                else if (ReservedWords.Operators.Contains(rpn.Peek().StringValue))
-                {
-                    operation = rpn.Pop().StringValue;
-
-                    if (a.Type == Value.ValueTypes.Pointer)
+                    try
                     {
-                        aname = a.StringValue;
-                        if (!line.Process.TryGetVariable(a.StringValue, out a))
+                        if (unaryOperators.Contains(token))
                         {
-                            ErrorBuilder.BuildError(line, ErrorBuilder.ErrorType.RuntimeError, "Variable \"" + a.StringValue + "\" undeclared.", ref err);
-                            errors = err.ToArray();
-                            return Value.NullValue;
+                            switch (token)
+                            {
+                                case "!":
+                                case "not":
+                                    result.Push(!b);
+                                    break;
+
+                                case "~":
+                                    result.Push(~b);
+                                    break;
+
+                                case "++":
+                                    result.Push(b++);
+                                    break;
+
+                                case "--":
+                                    result.Push(b--);
+                                    break;
+                            }
                         }
+                        else if (result.Count >= 1)
+                        {
+                            a = result.Pop();
+
+                            switch (token)
+                            {
+                                case "+":
+                                    result.Push(a + b);
+                                    break;
+
+                                case "-":
+                                    result.Push(a - b);
+                                    break;
+
+                                case "*":
+                                    result.Push(a * b);
+                                    break;
+
+                                case "/":
+                                    result.Push(a / b);
+                                    break;
+
+                                case "%":
+                                    result.Push(a % b);
+                                    break;
+
+                                case "^":
+                                    result.Push(Value.RaiseToPower(a, b));
+                                    break;
+
+                                case "|":
+                                case "or":
+                                    result.Push(a | b);
+                                    break;
+
+                                case "&":
+                                case "and":
+                                    result.Push(a & b);
+                                    break;
+
+                                case "=":
+                                case "==":
+                                    result.Push(a == b);
+                                    break;
+
+                                case "!=":
+                                    result.Push(a != b);
+                                    break;
+
+                                case "<":
+                                    result.Push(a < b);
+                                    break;
+
+                                case ">":
+                                    result.Push(a > b);
+                                    break;
+
+                                case "<=":
+                                    result.Push(a <= b);
+                                    break;
+
+                                case ">=":
+                                    result.Push(a >= b);
+                                    break;
+                            }
+                        }
+                        else if (token == "+")
+                        {
+                            result.Push(+b);
+                        }
+                        else if (token == "-")
+                        {
+                            result.Push(-b);
+                        }
+                        else
+                        {
+                            err.Add("Syntax error: invalid expression.");
+                            errors = err.ToArray();
+                            return 0;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        err.Add(ex.Message);
+                        errors = err.ToArray();
+                        return 0;
                     }
                 }
                 else
                 {
-                    ErrorBuilder.BuildError(line, ErrorBuilder.ErrorType.SyntaxError, "Invalid number of operands.", ref err);
-                    break;
-                }
-
-                if (a.IsNull) ErrorBuilder.BuildError(line, ErrorBuilder.ErrorType.RuntimeError, "NullReferenceException. Operand \"" + aname + "\" is NULL.", ref err);
-                if (b.IsNull) ErrorBuilder.BuildError(line, ErrorBuilder.ErrorType.RuntimeError, "NullReferenceException. Operand \"" + bname + "\" is NULL.", ref err);
-
-                Value c;
-
-                try
-                {
-                    switch (operation)
-                    {
-                        case "+":
-                            if (unary)
-                                c = +a;
-                            else
-                                c = a + b;
-                            break;
-
-                        case "-":
-                            if (unary)
-                                c = -a;
-                            else
-                                c = a - b;
-                            break;
-
-                        case "*":
-                            c = a * b;
-                            break;
-
-                        case "/":
-                            c = a / b;
-                            break;
-
-                        case "%":
-                            c = a % b;
-                            break;
-
-                        case "^":
-                            c = Value.RaiseToPower(a, b);
-                            break;
-
-                        case "&":
-                        case "and":
-                            c = a & b;
-                            break;
-
-                        case "|":
-                        case "or":
-                            c = a | b;
-                            break;
-
-                        case "!":
-                        case "not":
-                            c = !a;
-                            break;
-
-                        case "~":
-                            c = ~a;
-                            break;
-
-                        case "++":
-                            c = a++;
-                            break;
-
-                        case "--":
-                            c = a--;
-                            break;
-
-                    }
-                }
-                catch (InvalidOperationException ex)
-                {
-                    ErrorBuilder.BuildError(line, ErrorBuilder.ErrorType.RuntimeError, ex, ref err);
-                    break;
+                    result.Push(ConvertToValue(token, vars, functions));
                 }
             }
 
-            errors = err.ToArray();
-            return rpn.Pop();
+            if (result.Count == 1)
+            {
+                errors = err.ToArray();
+                return result.Pop();
+            }
+            else
+            {
+                err.Add("Syntax error: invalid expression.");
+                errors = err.ToArray();
+                return 0;
+            }
         }
 
-        private static Stack<Value> ConvertToRPN(string[] tokens)
+        private static Value ConvertToValue(string token, Dictionary<string, Value> vars, string[] function)
         {
-            Stack<Value> s = new Stack<Value>();
+            int itemp;
+            double ftemp;
+            bool btemp;
+            OrderedPair optemp;
+
+            if (ReservedWords.IsString(token))
+            {
+                return token.Trim('\"');
+            }
+            else if (int.TryParse(token, out itemp))
+            {
+                return itemp;
+            }
+            else if (double.TryParse(token, out ftemp))
+            {
+                return ftemp;
+            }
+            else if (bool.TryParse(token, out btemp))
+            {
+                return btemp;
+            }
+            else if (OrderedPair.TryParse(token, out optemp))
+            {
+                return new Value(optemp);
+            }
+            else if (vars.ContainsKey(token))
+            {
+                return vars[token];
+            }
+            else if (function.Contains(token))
+            {
+                // TODO: evaluate function
+                return "";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Converts the given infix expression (as a token array) into an postfix expression (as a queue string)
+        /// </summary>
+        /// <param name="tokens"></param>
+        /// <param name="vars"></param>
+        /// <param name="errors"></param>
+        /// <returns></returns>
+        private static Queue<string> ConvertToPostfix(string[] tokens, Dictionary<string, Value> vars, out string[] errors)
+        {
+            Queue<string> output = new Queue<string>();
+            Stack<string> s = new Stack<string>();
+
+            List<string> err = new List<string>();
+
+            double dtemp;
+            bool btemp;
+            OrderedPair otemp;
 
             foreach (string token in tokens)
             {
-                
+                if (ReservedWords.Operators.Contains(token))
+                {
+                    string[] rightAssc = new string[] { "!", "~", "not", ",", "^"};
+                    while ((s.Count > 0) && (ReservedWords.Operators.Contains(s.Peek())) &&
+                        (((!rightAssc.Contains(token)) && (ReservedWords.GetPrecedence(token) == ReservedWords.GetPrecedence(s.Peek()))) ||
+                        (ReservedWords.GetPrecedence(token) < ReservedWords.GetPrecedence(s.Peek()))))
+                    {
+                        output.Enqueue(s.Pop());
+                    }
+                    s.Push(token);
+                }
+                else if (double.TryParse(token, out dtemp) || bool.TryParse(token, out btemp) || OrderedPair.TryParse(token, out otemp))
+                {
+                    output.Enqueue(token);
+                }
+                else if (ReservedWords.IsString(token))
+                {
+                    output.Enqueue(token);
+                }
+                else if (vars.ContainsKey(token))
+                {
+                    output.Enqueue(token);
+                }
+                else if (token == ")")
+                {
+                    while ((s.Count > 0) && (s.Peek() != "(")) 
+                    {
+                        output.Enqueue(s.Pop());
+                        if (s.Count == 0)
+                            err.Add("Parenthesis mismatch. Missing '('");
+                    }
+                    if ((s.Count > 0) && (s.Peek() == "("))
+                        s.Pop();
+                }
+                else if (token == "]")
+                {
+                    while ((s.Count > 0) && (s.Peek() != "["))
+                    {
+                        output.Enqueue(s.Pop());
+                        if (s.Count == 0)
+                            err.Add("Square bracket mismatch. Missing '['");
+                    }
+                    if ((s.Count > 0) && (s.Peek() == "["))
+                        output.Enqueue(s.Pop());
+                }
+                else if (token == "(" | token == "[")
+                {
+                    s.Push(token);
+                }
+                else
+                {
+                    s.Push(token);
+                }
             }
 
-            return s;
+            while (s.Count > 0)
+            {
+                if ((s.Peek() == "(") | (s.Peek() == ")"))
+                    err.Add("Parenthesis mismatch.");
+                else if ((s.Peek() == "]") | (s.Peek() == "["))
+                    err.Add("Square bracket mismatch.");
+                else
+                    output.Enqueue(s.Pop());
+            }
+
+            errors = err.ToArray();
+            return output;
         }
     }
 }
