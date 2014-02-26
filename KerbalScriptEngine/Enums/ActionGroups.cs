52695c844ade214f060e1ef6a3f8a646b900a4e9
David Grider <woodywood245@yahoo.com>
2014-02-26 15:11:28 -0800

    Added action groups

---
+++ KerbalScriptEngine/Enums/ActionGroups.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerboScriptEngine.Enums
+{
+    enum ActionGroups
+    {
+        AG1,
+        AG2,
+        AG3,
+        AG4,
+        AG5,
+        AG6,
+        AG7,
+        AG8,
+        AG9,
+        AG0,
+        SAS
+    }
+}
