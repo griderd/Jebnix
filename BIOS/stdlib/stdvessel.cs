447956d4cde2f59ff15c9e964422a822ed77bf37
DavidGrider <woodywood245@yahoo.com>
2014-03-06 02:35:26 -0800

    Added easy access to PartResources via stdvessel.

--- BIOS/stdlib/stdvessel.cs
+++ BIOS/stdlib/stdvessel.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using KSP;
 
 namespace BIOS.stdlib
 {
     public class stdvessel
     {
+        public static PartResource[] GetResources(Vessel vessel)
+        {
+            List<PartResource> resources = new List<PartResource>();
+
+            Func<string, bool> ContainsResource = delegate(string name)
+            {
+                foreach (PartResource r in resources)
+                {
+                    if (r.resourceName == name)
+                        return true;
+                }
+                return false;
+            };
+
+            foreach (Part p in vessel.Parts)
+            {
+                foreach (PartResource r in p.Resources)
+                {
+                    if (!ContainsResource(r.resourceName))
+                        resources.Add(r);
+                }
+            }
+
+            return resources.ToArray();
+        }
+
+        public static PartResource[] GetResources(Interpreter interpreter)
+        {
+            return GetResources(interpreter.vessel);
+        }
     }
 }
