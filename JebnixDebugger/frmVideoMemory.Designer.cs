6393449450fa49bd317e12aad64cbaf8fca84a28
DavidGrider <woodywood245@yahoo.com>
2014-07-02 19:00:47 -0700

    Improved video memory viewer.

--- JebnixDebugger/frmVideoMemory.Designer.cs
+++ JebnixDebugger/frmVideoMemory.Designer.cs
@@ -29,24 +29,53 @@
         private void InitializeComponent()
         {
             this.txtMemory = new System.Windows.Forms.TextBox();
+            this.txtHexView = new System.Windows.Forms.TextBox();
+            this.chkHexDigits = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // txtMemory
             // 
-            this.txtMemory.Location = new System.Drawing.Point(17, 15);
+            this.txtMemory.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMemory.Location = new System.Drawing.Point(12, 12);
             this.txtMemory.Multiline = true;
             this.txtMemory.Name = "txtMemory";
-            this.txtMemory.Size = new System.Drawing.Size(550, 452);
+            this.txtMemory.Size = new System.Drawing.Size(392, 428);
             this.txtMemory.TabIndex = 0;
+            this.txtMemory.Text = "This isntance is being run outside of KSP.";
+            // 
+            // txtHexView
+            // 
+            this.txtHexView.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHexView.Location = new System.Drawing.Point(410, 12);
+            this.txtHexView.Multiline = true;
+            this.txtHexView.Name = "txtHexView";
+            this.txtHexView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHexView.Size = new System.Drawing.Size(732, 428);
+            this.txtHexView.TabIndex = 1;
+            // 
+            // chkHexDigits
+            // 
+            this.chkHexDigits.AutoSize = true;
+            this.chkHexDigits.Location = new System.Drawing.Point(410, 446);
+            this.chkHexDigits.Name = "chkHexDigits";
+            this.chkHexDigits.Size = new System.Drawing.Size(74, 17);
+            this.chkHexDigits.TabIndex = 2;
+            this.chkHexDigits.Text = "Hex Digits";
+            this.chkHexDigits.UseVisualStyleBackColor = true;
+            this.chkHexDigits.CheckedChanged += new System.EventHandler(this.chkHexDigits_CheckedChanged);
             // 
             // frmVideoMemory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 492);
+            this.ClientSize = new System.Drawing.Size(1154, 496);
+            this.Controls.Add(this.chkHexDigits);
+            this.Controls.Add(this.txtHexView);
             this.Controls.Add(this.txtMemory);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "frmVideoMemory";
-            this.Text = "Form1";
+            this.Text = "Video Memory";
             this.Load += new System.EventHandler(this.frmVideoMemory_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -56,6 +85,8 @@
         #endregion
 
         internal System.Windows.Forms.TextBox txtMemory;
+        internal System.Windows.Forms.TextBox txtHexView;
+        private System.Windows.Forms.CheckBox chkHexDigits;
 
 
     }
--- JebnixDebugger/frmVideoMemory.cs
+++ JebnixDebugger/frmVideoMemory.cs
@@ -23,8 +23,34 @@ namespace kOSDebugger
 
         public void RefreshText()
         {
+            this.Text = "Video Memory (" + Jebnix.Graphics.Graphics.TextWidth.ToString() + "x" + Jebnix.Graphics.Graphics.TextHeight.ToString() + ")";
             txtMemory.Text = Jebnix.Graphics.Graphics.Text;
+
+            StringBuilder hex = new StringBuilder();
+            for (int i = 0; i < txtMemory.Text.Length; i++)
+            {
+                if (i % 16 == 0)
+                    hex.Append(i.ToString("X3"));
+                hex.Append("  ");
+                if (chkHexDigits.Checked)
+                    hex.Append(((int)txtMemory.Text[i]).ToString("X2"));
+                else
+                {
+                    hex.Append(' ');
+                    hex.Append(txtMemory.Text[i]);
+                }
+
+                if ((i + 1) % 16 == 0)
+                    hex.AppendLine();
+            }
+            txtHexView.Text = hex.ToString();
+
             txtMemory.Invalidate();
+        }
+
+        private void chkHexDigits_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshText();
         }
     }
 }
